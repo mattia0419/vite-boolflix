b79f97dcf5b8565c8d015925c0a10a274d41824e
mattia0419 <pironemattia6@gmail.com>
2023-09-14 16:36:16 +0200

    add structure

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import { store } from './data/store';
+export default {
+  data() {
+    return {
+      store
+    }
+  }
+}
+
+</script>
+
+
+<template>
+  <h1>{{ store.title }}</h1>
+</template>
+
+
+<style lang="scss">
+@use './assets/style.scss'
+</style>
